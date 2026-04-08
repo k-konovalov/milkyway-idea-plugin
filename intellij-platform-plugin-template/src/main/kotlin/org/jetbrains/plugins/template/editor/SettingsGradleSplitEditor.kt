a92d7de8e4f503dc748c022c241381165a757c56
Nikita Rakcheev <nrakcheev4@gmail.com>
2026-04-08 16:49:15 +0300

    F-32: Add Basic Splitter, Action and Startup.

---
+++ intellij-platform-plugin-template/src/main/kotlin/org/jetbrains/plugins/template/editor/SettingsGradleSplitEditor.kt
@@ -0,0 +1,16 @@
+package org.jetbrains.plugins.template.editor
+
+import com.intellij.openapi.fileEditor.TextEditor
+import com.intellij.openapi.fileEditor.TextEditorWithPreview
+import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+
+class SettingsGradleSplitEditor (
+    project: Project,
+    file: VirtualFile
+) : TextEditorWithPreview(
+    TextEditorProvider.getInstance().createEditor(project, file) as TextEditor,
+    SettingsGradlePreviewEditor(project, file),
+    "Settings Gradle Split Editor Graph"
+)
