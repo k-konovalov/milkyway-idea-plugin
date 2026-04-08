52ea1525693df237996ba931c5b3cedb1845a40b
Nikita Rakcheev <nrakcheev4@gmail.com>
2026-04-08 16:49:15 +0300

    F-32: Add Basic Splitter, Action and Startup.

--- intellij-platform-plugin-template/src/main/kotlin/org/jetbrains/plugins/template/services/MyProjectService.kt
+++ intellij-platform-plugin-template/src/main/kotlin/org/jetbrains/plugins/template/services/MyProjectService.kt
@@ -3,15 +3,24 @@ package org.jetbrains.plugins.template.services
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VfsUtilCore
 import org.jetbrains.plugins.template.MyBundle
 
 @Service(Service.Level.PROJECT)
-class MyProjectService(project: Project) {
-
-    init {
-        thisLogger().info(MyBundle.message("projectService", project.name))
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
-    }
+class MyProjectService(private val project: Project) {
 
     fun getRandomNumber() = (1..100).random()
+
+    fun printSettingsGradleFiles() {
+        thisLogger().debug("project.baseDir: " + project.baseDir)
+
+        val baseDir = project.baseDir ?: return
+        val searchFiles = listOf("build.gradle.kts", "settings.gradle.kts")
+        VfsUtilCore.iterateChildrenRecursively(baseDir, null) { file ->
+            if (file.name in searchFiles) {
+                println(file.canonicalPath)
+            }
+            true
+        }
+    }
 }
