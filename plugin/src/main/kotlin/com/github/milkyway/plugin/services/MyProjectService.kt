5e39a9db7f9d353a73026cac201e3761daf32d06
Nikita Rakcheev <nrakcheev4@gmail.com>
2026-04-15 00:22:11 +0300

    F-32: Namespaces were Changed. Redundant Files were Removed.

---
+++ plugin/src/main/kotlin/com/github/milkyway/plugin/services/MyProjectService.kt
@@ -0,0 +1,26 @@
+package com.github.milkyway.plugin.services
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VfsUtilCore
+import com.github.milkyway.plugin.MyBundle
+
+@Service(Service.Level.PROJECT)
+class MyProjectService(private val project: Project) {
+
+    fun getRandomNumber() = (1..100).random()
+
+    fun printSettingsGradleFiles() {
+        thisLogger().debug("project.baseDir: " + project.baseDir)
+
+        val baseDir = project.baseDir ?: return
+        val searchFiles = listOf("build.gradle.kts", "settings.gradle.kts")
+        VfsUtilCore.iterateChildrenRecursively(baseDir, null) { file ->
+            if (file.name in searchFiles) {
+                println(file.canonicalPath)
+            }
+            true
+        }
+    }
+}
