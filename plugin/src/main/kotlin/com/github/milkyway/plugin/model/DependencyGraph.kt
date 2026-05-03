8d6d2df0e2be4af4147ce82d235555712f908073
Nikita Rakcheev <nrakcheev4@gmail.com>
2026-05-03 23:51:55 +0300

    F-24: Metrics: Percent of equality to form (algo)

--- plugin/src/main/kotlin/com/github/milkyway/plugin/model/DependencyGraph.kt
+++ plugin/src/main/kotlin/com/github/milkyway/plugin/model/DependencyGraph.kt
@@ -49,7 +49,7 @@ class DependencyGraph() {
         return builder.toString()
     }
 
-    fun moduleColor(module: String): String = when {
+    private fun moduleColor(module: String): String = when {
         module.startsWith(":feature") -> "lightblue"
         module.startsWith(":core") -> "lightgray"
         module.startsWith(":model") -> "lightyellow"
@@ -62,5 +62,57 @@ class DependencyGraph() {
         val articulationPointsGraph = ArticulationPointsGraph(indirectRawEdges)
         articulationPoints = articulationPointsGraph.findArticulationPoints().toMutableList()
         print(articulationPoints)
+    }
+
+    fun buildLayerProfile(): List<Int> {
+        val indegree = mutableMapOf<Node, Int>().withDefault { 0 }
+
+        for (node in nodes.values) {
+            indegree.putIfAbsent(node, 0)
+        }
+
+        for ((_, children) in adj) {
+            for (child in children) {
+                indegree[child] = indegree.getValue(child) + 1
+            }
+        }
+
+        // region Topological Traversal
+        val queue = ArrayDeque<Node>()
+        val layer = mutableMapOf<Node, Int>()
+
+        for ((node, deg) in indegree) {
+            if (deg == 0) {
+                queue.add(node)
+                layer[node] = 0
+            }
+        }
+        var processed = 0
+        while (queue.isNotEmpty()) {
+            val from = queue.removeFirst()
+            ++processed
+            for (to in adj[from].orEmpty()) {
+                val newLayer = layer.getValue(from) + 1
+                layer[to] = maxOf(layer.getOrDefault(to, 0), newLayer)
+                indegree[to] = indegree.getValue(to) - 1;
+                if (indegree[to] == 0) {
+                    queue.add(to)
+                }
+            }
+        }
+        if (processed != nodes.size) {
+            error("Graph contains cycles. Expected DAG")
+        }
+        // endregion
+        val counts = mutableMapOf<Int, Int>().withDefault { 0 }
+        for (node in nodes.values) {
+            val l = layer.getValue(node)
+            counts[l] = counts.getValue(l) + 1
+        }
+
+        val maxLayer = counts.keys.maxOrNull() ?: 0
+        return List(maxLayer + 1) { i ->
+            counts.getOrDefault(i, 0)
+        }
     }
 }
