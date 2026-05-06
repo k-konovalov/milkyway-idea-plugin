bae684682ea486eec20e3f83f6ab621251825467
Nikita Rakcheev <nrakcheev4@gmail.com>
2026-05-06 15:59:12 +0300

    F-30: Basic Settings Template. Gradle/Regex Selection Enabled.

--- milkyway-plugin/idea-plugin/src/main/kotlin/com/github/milkyway/idea/GradleDependencyAnalysisRunner.kt
+++ milkyway-plugin/idea-plugin/src/main/kotlin/com/github/milkyway/idea/GradleDependencyAnalysisRunner.kt
@@ -1,16 +1,18 @@
 package com.github.milkyway.idea
 
-import com.github.milkyway.core.analyzer.ArticulationPointsAnalyzer
-import com.github.milkyway.core.shape.GraphShapeMatcher
+import com.github.milkyway.core.models.DependencyGraph
 import com.github.milkyway.idea.cytoscape.ReportBuilder
+import com.github.milkyway.idea.resolver.DependencyResolver
 import com.github.milkyway.idea.resolver.GradleDependencyResolver
 import com.github.milkyway.idea.resolver.RegexDependencyResolver
+import com.github.milkyway.idea.settings.MilkyWaySettings
 import com.intellij.openapi.project.Project
 import kotlinx.serialization.json.Json
 import java.io.File
 
 class GradleDependencyAnalysisRunner(
     private val ideaProject: Project,
+    private val settings: MilkyWaySettings = MilkyWaySettings.getInstance(),
 ) {
     private val json = Json {
         prettyPrint = true
@@ -20,15 +22,16 @@ class GradleDependencyAnalysisRunner(
 
     fun run(projectDir: File): String {
         println("Gradle Traverse started")
-//        val gradleDependencyResolver = GradleDependencyResolver(ideaProject, projectDir)
-//        var graph = gradleDependencyResolver.resolve()
-//        graph.adjacency.forEach { (module, children) ->
-//            println("${module}: [${children}]")
-//        }
-
-        println("Regext Traverse Started")
-        val regexDependencyResolver = RegexDependencyResolver(ideaProject)
-        val graph = regexDependencyResolver.resolve()
+        val isGradleParser = settings.state.parser == MilkyWaySettings.PARSER_GRADLE
+        println("settings.state ${settings.state}")
+        val dependencyResolver: DependencyResolver = if (isGradleParser) {
+            println("Parsing by gradle")
+            GradleDependencyResolver(ideaProject, projectDir)
+        } else {
+            println("Paring by regex")
+            RegexDependencyResolver(ideaProject)
+        }
+        val graph: DependencyGraph = dependencyResolver.resolve()
         graph.adjacency.forEach { (module, children) ->
             println("${module}: [${children}]")
         }
