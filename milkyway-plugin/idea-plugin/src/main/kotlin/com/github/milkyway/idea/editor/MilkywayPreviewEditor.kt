6ae2e559c682465fa817563e9b98a0c593be5e3e
Nikita Rakcheev <nrakcheev4@gmail.com>
2026-06-10 15:44:28 +0300

    F-76: Each splite editor has own browser. Reload on file change. Fix reading cached file text.

--- milkyway-plugin/idea-plugin/src/main/kotlin/com/github/milkyway/idea/editor/MilkywayPreviewEditor.kt
+++ milkyway-plugin/idea-plugin/src/main/kotlin/com/github/milkyway/idea/editor/MilkywayPreviewEditor.kt
@@ -8,9 +8,12 @@ import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorState
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.UserDataHolderBase
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.readText
 import com.intellij.ui.jcef.JBCefBrowser
+import com.intellij.util.Alarm
 import org.jetbrains.annotations.Nls
 import java.awt.BorderLayout
 import java.beans.PropertyChangeListener
@@ -20,14 +23,43 @@ import javax.swing.SwingUtilities
 
 class MilkywayPreviewEditor(
     private val project: Project,
+    private val file: VirtualFile,
 ): UserDataHolderBase(), FileEditor {
     private val panel = JPanel(BorderLayout())
     private val browser = JBCefBrowser()
     private val settings = MilkyWaySettings.getInstance()
+    private val document = FileDocumentManager.getInstance().getDocument(file) ?:
+        error("Can't get document for file ${file.path}")
+    private var dependencySet: Set<String> = mutableSetOf()
+    private var includeSet: Set<String> = mutableSetOf()
+
+    private var alarm = Alarm(Alarm.ThreadToUse.POOLED_THREAD, this)
+    private val documentListener = object : DocumentListener {
+        override fun documentChanged(event: DocumentEvent) {
+            alarm.cancelAllRequests()
+
+            val text = event.document.text
+            val newDependencySet = parseModuleDeps(text)
+            val newIncludeSet = parseSettingsDeps(text)
+
+            if (newDependencySet == dependencySet && newIncludeSet == includeSet) {
+                return
+            }
+            dependencySet = newDependencySet
+            includeSet = newIncludeSet
+            alarm.addRequest({
+                val startupGraphAnalysis = StartupGraphAnalysis(project, file, this@MilkywayPreviewEditor)
+                startupGraphAnalysis.run()
+            }, 1000)
+        }
+    }
 
     init {
+        dependencySet = parseModuleDeps(file.readText())
+        includeSet = parseSettingsDeps(file.readText())
         reload(null)
         panel.add(browser.component, BorderLayout.CENTER)
+        document.addDocumentListener(documentListener)
     }
 
     fun reload(json: String?) {
@@ -43,14 +75,7 @@ class MilkywayPreviewEditor(
         }
     }
 
-//    private val documentListener = object : DocumentListener {
-//        override fun documentChanged(event: DocumentEvent) {
-//            updatePreview();
-//        }
-//    }
-
-    private fun updatePreview() {
-    }
+    // region Mostly unused
     override fun getComponent(): JComponent = panel
 
     override fun getPreferredFocusedComponent(): JComponent? = panel
@@ -66,8 +91,32 @@ class MilkywayPreviewEditor(
     override fun addPropertyChangeListener(p0: PropertyChangeListener) { }
 
     override fun removePropertyChangeListener(p0: PropertyChangeListener) { }
+    // endregion
 
+    // region Parse all dependencies
+    companion object {
+        private val includeRegex = Regex("""include\s*\(["'](:[^"']+)["']\s*\)""")
+        private val dependencyRegex = Regex(
+            """(\w+)\s*\(\s*project\s*\(\s*["'](:[^"']+)["']\s*\)\s*\)""",
+            setOf(RegexOption.DOT_MATCHES_ALL)
+        )
+    }
+
+    private fun parseSettingsDeps(settingsText: String): Set<String> {
+        return includeRegex.findAll(settingsText)
+            .map { it.groupValues[1] }
+            .toSet()
+    }
+
+    private fun parseModuleDeps(buildText: String): Set<String> {
+        return dependencyRegex.findAll(buildText)
+            .map { it.groupValues[2] }.toSet()
+    }
+
+    // endregion
     override fun dispose() {
-//        document.removeDocumentListener(documentListener)
+        println("__Disposing preview editor ${file.path}")
+        document.removeDocumentListener(documentListener)
+        Disposer.dispose(browser)
     }
 }
